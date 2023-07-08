a2dc4597169ea4d1f4fa192fcefd7b40bf9479d0
Andrew Alsing <andrew32alsing@gmail.com>
2023-07-07 17:57:50 -0700

    added cards

--- scripts.js
+++ scripts.js
@@ -6,7 +6,25 @@
 // /orgs: Retrieves the list of organizations that the user is a member of.
 // /received_events: Retrieves the public activity events that the user has received.
 
-fetch("https://api.github.com/users/andrew32a/repos")
+fetch("https://api.github.com/users/andrew32a/events")
   .then((response) => response.json())
-  .then((data) => console.log(data))
+  .then((data) => {
+    // Create a card for each data item
+    data.forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+
+      // Add data to the card
+      const actorLogin = document.createElement("p");
+      actorLogin.textContent = "Actor Login: " + item.actor.login;
+      card.appendChild(actorLogin);
+
+      const repoName = document.createElement("p");
+      repoName.textContent = "Repository Name: " + item.repo.name;
+      card.appendChild(repoName);
+
+      // Add the card to the container
+      document.getElementById("cardContainer").appendChild(card);
+    });
+  })
   .catch((error) => console.log(error));
